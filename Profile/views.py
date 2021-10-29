498d7acccd9098ab835d2fc608567ee9a5bdc492
dandmish <40658318+dandmish@users.noreply.github.com>
2021-10-29 14:53:24 +0300

    Add Profile/info (#8)

--- Profile/views.py
+++ Profile/views.py
@@ -1,6 +1,17 @@
+from asgiref.sync import sync_to_async
 from django.http import JsonResponse
+from BaseAsyncClassView import AsyncView
+from util.auth import authenticated
+from Profile.models import Profile
 
 
 # Create your views here.
-def test(request, user_id: str) -> JsonResponse:
-    return JsonResponse(data={'user_if': user_id}, safe=False)
+class GetProfileInfo(AsyncView):
+    # @authenticated
+    @sync_to_async
+    def get(self, request) -> JsonResponse:
+        user_id = 1
+        profile = Profile.objects.get(id=user_id)
+        data = {"user_name": profile.user_name,
+                "email": profile.email}
+        return JsonResponse(data=data, safe=False)
