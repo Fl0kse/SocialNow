361f28104e3a9972848d6a12093900d2a7b5a922
Fl0kse <ulartem2828@gmail.com>
2021-10-29 11:58:46 +0300

    Some fix

--- util/requests.py
+++ util/requests.py
@@ -1,7 +1,5 @@
-import aiohttp
-import asyncio
 from typing import Dict, Any
-from rest_framework.views import APIView
+import aiohttp
 
 
 async def async_request(url: str, method: str, headers: Dict[str: str] = None, json: Dict[Any, Any] = None):
